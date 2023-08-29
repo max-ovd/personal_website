3cba6f3d9aea7d28f6f2da490176e0c54ed30575
Max <4max.ovd@gmail.com>
2023-08-29 15:00:07 -0400

    updated index.html with navbar and started styles.css

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route ("/")
+@app.route("/")
 def index():
     return render_template('index.html')
 
+if __name__ == "__main__":
+    app.run(debug=True)
