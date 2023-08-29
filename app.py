95a3dfb6fbc016e9eb984b1a18a349f303bef501
Max <4max.ovd@gmail.com>
2023-08-29 17:26:19 -0400

    added weather and time

--- app.py
+++ app.py
@@ -1,11 +1,49 @@
 from flask import Flask, request, render_template
+import requests
+import geocoder
+from geopy.geocoders import Nominatim
+from datetime import datetime
 
 
 app = Flask(__name__)
 
+# initialize api
+geolocator = Nominatim(user_agent="geoapiExercises")
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+    city, country, temp = get_location()
+    time = get_time()
+    return render_template('index.html', city=city, country=country, temp=temp, time=time)
 
+
+
+
+def get_location():
+    api_key = "30d4741c779ba94c470ca1f63045390a"
+
+    # find city, country
+    g = geocoder.ip('me')
+    location = geolocator.reverse(str(g.lat) + "," + str(g.lng)).raw['address']
+    city = location.get('city')
+    country = location.get('country')
+    if city == 'Old Toronto':
+        city = 'toronto'
+
+    # request data openweather api
+    url = (f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&APPID={api_key}")
+    res = requests.get(url)
+    data = res.json()
+    temp = data['main']['temp']
+
+
+
+    return (city, country, temp)
+
+def get_time():
+    currentDateAndTime = datetime.now()
+    currentTime = currentDateAndTime.strftime("%H:%M")
+    return currentTime
+    
 if __name__ == "__main__":
     app.run(debug=True)
